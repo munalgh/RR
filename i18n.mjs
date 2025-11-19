23cea9f48145a1844c24bf26d651b68de3cbabc4
Muna <munalgh.598@gmail.com>
2025-11-19 13:57:08 +0300

    Delete i18n.mjs

--- i18n.mjs
+++
@@ -1,9 +0,0 @@
-import { createI18n } from 'vue-i18n'
-import en from './locales/en.json'
-import ar from './locales/ar.json'
-
-export default createI18n({
-  legacy: false,
-  locale: localStorage.getItem('lang') || 'en',
-  messages: { en, ar }
-})
